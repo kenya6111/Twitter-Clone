24633278fce2dcfd681f2f12a164c2142888e9a5
kenya6111 <kenyanke6111@gmail.com>
2025-01-17 21:28:33 +0900

    model,viewファイル修正

--- twitter_clone/models.py
+++ twitter_clone/models.py
@@ -1,5 +1,7 @@
 # import random
 from django.db import models
+import re
+from django.core.exceptions import ObjectDoesNotExist
 
 from django.contrib.auth.models import AbstractUser
 # Create your models here.
@@ -14,6 +16,23 @@ class CustomUser(AbstractUser):
     tel = models.EmailField(max_length=100,blank=True, null=True)
     date_of_birth = models.EmailField(max_length=100,blank=True, null=True)
     # age = models.IntegerField('年齢', blank=True, null=True)
+
+    @classmethod
+    def is_valid_email(email):
+        regex = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+        return bool(re.match(regex, email))
+
+    @classmethod
+    def is_valid_phone_number(phone_number):
+        phone_number_pattern = r"^0[789]0\d{8}$"
+        return bool(re.match(phone_number_pattern, phone_number))
+    @classmethod
+    def get_user_from_session(cls, session):
+        """セッションからユーザを取得する"""
+        user_id = session.get('user_id')
+        if not user_id:
+            raise ObjectDoesNotExist("セッションにユーザIDがありません。")
+        return cls.objects.get(id=user_id)
 
 
 class EmailVerificationModel(models.Model):
