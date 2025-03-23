76ba301ca2cf077bec931dd3396edf90b02c64d8
kenya6111 <kenyanke6111@gmail.com>
2025-03-23 19:40:05 +0900

    返信POST時の処理を追加

--- twitter_clone/views.py
+++ twitter_clone/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render,redirect
 from django.urls import reverse
 from django.contrib import messages
 from django.core.mail import send_mail
-from twitter_clone.models import CustomUser, TweetModel
+from twitter_clone.models import CustomUser, TweetModel,ReplyModel
 from django.core.exceptions import ObjectDoesNotExist
 import secrets
 from django.contrib.auth.hashers import make_password,check_password
@@ -194,7 +194,7 @@ def profile_view(request):
     if filter_type == 'post':
         tweet_list = TweetModel.objects.filter(user=custom_user).order_by('-created_at')
     elif filter_type == 'comment':
-        tweet_list = TweetModel.objects.filter(replies__user=custom_user).order_by('-created_at')
+        tweet_list = TweetModel.objects.filter(reply_tweets__user=custom_user).order_by('-created_at')
     elif filter_type == 'retweet':
         tweet_list = TweetModel.objects.filter(retweets__user=custom_user).order_by('-created_at')
     elif filter_type == 'like':
@@ -240,41 +240,47 @@ def profile_edit_view(request):
 
     return render(request, 'twitter_clone/profile_edit.html', {'custom_user':custom_user})
 def tweet_detail_view(request):
+    if request.method == 'POST':
+        user = request.user
+        login_user = CustomUser.objects.get(id=user.id)
+
+        origin_tweet_id = request.POST.get("tweet_id", None)
+        tweet = TweetModel.objects.get(id=origin_tweet_id)
+
+        tweet_sentence = request.POST.get("tweet-sentence", None)
+        tweet_image=""
+        if 'tweet-image' in request.FILES:
+            uploaded_tweet_image = upload(request.FILES.get('tweet-image'))
+            tweet_image = uploaded_tweet_image['secure_url']
+        reply_tweet = TweetModel.objects.create(user=login_user, sentense=tweet_sentence, image=tweet_image)
+        ReplyModel.objects.create(user=tweet.user, origin_tweet=tweet, reply_tweet=reply_tweet)
+
+
+        url = reverse('tweet_detail')
+        parameters = urlencode({"tweet_id":origin_tweet_id})
+        return redirect(f'{url}?{parameters}')
+
     tweet_id = request.GET.get("tweet_id")
+
+    # 元ツイートを取得
     tweet = TweetModel.objects.get(id=tweet_id)
 
+    # ログインユーザを取得
     user = request.user
     custom_user = CustomUser.objects.get(id=user.id)
 
-    # if request.method == 'POST':
-    #     try:
-    #         user_id = request.POST.get("user_id", None)
-    #         custom_user = CustomUser.objects.get(id=user_id)
-    #         custom_user.username = request.POST.get("username", None)
-    #         custom_user.introduction = request.POST.get("introduction", None)
-    #         custom_user.place = request.POST.get("place", None)
-    #         custom_user.web_site = request.POST.get("web_site", None)
-    #         custom_user.birth = request.POST.get("birth", None)
-    #         if 'head-image' in request.FILES:
-    #             uploaded_head_image = upload(request.FILES['head-image'])
-    #             custom_user.head_image = uploaded_head_image['secure_url']
-    #         if 'image' in request.FILES:
-    #             uploaded_image = upload(request.FILES['image'])
-    #             custom_user.image = uploaded_image['secure_url']
-    #         custom_user.save()
-    #         redirect_url = reverse('profile')
-    #         parameters = urlencode({'user_id': user_id})
-    #         url = f'{redirect_url}?{parameters}'
+    # リプライのツイートを取得
+    reply_list = ReplyModel.objects.filter(origin_tweet=tweet).order_by('-created_at')
 
-    #         return redirect(url)
-    #     except Exception:
-    #         return render(request,'twitter_clone/profile.html')
-    # else:
-    #     custom_user = CustomUser.objects.get(id=user_id)
+    data_page = Paginator(reply_list, 2)
+    p = request.GET.get('p')
+
+    replies = data_page.get_page(p)
+
 
 
     # return render(request, 'twitter_clone/profile_edit.html', {'custom_user':custom_user})
-    return render(request, 'twitter_clone/tweet_detail.html',{"article":tweet, "login_user":custom_user})
+    return render(request, 'twitter_clone/tweet_detail.html',{"article":tweet, "login_user":custom_user,"replies":replies})
 
 def tweet_view(request):
     if request.method == 'POST':
